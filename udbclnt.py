f7c7a64392e98db94982e9c90937574dc3541c67
mohamed maged <mohamedmaged35@yahoo.com>
2017-11-30 12:44:43 +0200

    remove checksum

--- udbclnt.py
+++ udbclnt.py
@@ -4,25 +4,19 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSocket.settimeout(10)
-sentAck = "koko,0,ACK"
+sentAck = "0,ACK"
 
 seqSnd = 0
 seqRcvd = 0
 
 
-def verifyCheckSum(packet):
-    packet = packet.decode("UTF-8")
-    arr = packet.split(",")
-    if arr[0] == getCheckSum(arr[2]):
-        return 1
-    else:
-        return 0
+
 
 
 def isACK(packet):
     packet = packet.decode("UTF-8")
     arr = packet.split(",")
-    if arr[2] == "ACK" and int(arr[1]) == seqSnd:
+    if arr[1] == "ACK" and int(arr[0]) == seqSnd:
         return 1
     else:
         return 0
@@ -32,7 +26,7 @@ def verifySeq(packet):
     global seqRcvd
     packet = packet.decode("UTF-8")
     arr = packet.split(",")
-    if int(arr[1]) == seqRcvd:
+    if int(arr[0]) == seqRcvd:
         return 1
     else:
         return 0
@@ -43,14 +37,14 @@ def receive():
     global seqRcvd
     data, clientAddress = clientSocket.recvfrom(2048)
     print("Packet Recived : " + data.decode('UTF-8'))
-    if (verifyCheckSum(data) and verifySeq(data)):
+    if verifySeq(data):
         sentAck = createPacket("ACK", seqRcvd)
         print("ACK to send: " + sentAck)
         clientSocket.sendto(sentAck.encode('UTF-8'), (serverName, serverPort))
         data = data.decode("UTF-8")
         arr = data.split(",")
         seqRcvd = 1 - seqRcvd
-        return arr[2]
+        return arr[1]
     clientSocket.sendto(sentAck.encode('UTF-8'), (serverName, serverPort))
     return receive()
 
@@ -62,7 +56,7 @@ def sendPacket(packet):
         clientSocket.sendto(packet.encode('UTF-8'), (serverName, serverPort))
         ack = clientSocket.recv(2048)
         print("ACK recived : " + ack.decode("UTF-8"))
-        if not (verifyCheckSum(ack) and isACK(ack)):
+        if not (isACK(ack)):
             ack = clientSocket.recv(2048)
         else:
             seqSnd = 1 - seqSnd
@@ -70,14 +64,11 @@ def sendPacket(packet):
         sendPacket(packet)
 
 
-def getCheckSum(data):
-    return "koko"
-
 
 def createPacket(data, seq):
     print(data)
-    checkSum = getCheckSum(data)
-    packet = checkSum + ',' + str(seq) + ',' + str(data)
+
+    packet = str(seq) + ',' + str(data)
     return packet
 
 
--- udbsrv.py
+++ udbsrv.py
@@ -1,7 +1,7 @@
 import socket
 
 ack = "ACK"
-sentAck = "koko,0,ACK"
+sentAck = "0,ACK"
 serverPort = 12000
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
@@ -12,20 +12,12 @@ while 1:
     try:
         serverSocket.settimeout(10)
 
-        def verifyCheckSum(packet):
-            packet = packet.decode("UTF-8")
-            arr = packet.split(",")
-            if arr[0] == getCheckSum(arr[2]):
-                return 1
-            else:
-                return 0
-
 
         def isACK(packet):
             global seqSnd
             packet = packet.decode("UTF-8")
             arr = packet.split(",")
-            if arr[2] == "ACK" and int(arr[1]) == seqSnd:
+            if arr[1] == "ACK" and int(arr[0]) == seqSnd:
                 return 1
             else:
                 return 0
@@ -35,7 +27,7 @@ while 1:
             global seqRcvd
             packet = packet.decode("UTF-8")
             arr = packet.split(",")
-            if int(arr[1])== seqRcvd:
+            if int(arr[0]) == seqRcvd:
                 return 1
             else:
                 return 0
@@ -46,15 +38,15 @@ while 1:
             global seqRcvd
             data, clientAddress = serverSocket.recvfrom(2048)
             print("packet received: " + data.decode("UTF-8"))
-            if (verifyCheckSum(data) and verifySeq(data)):
+            if verifySeq(data):
                 sentAck = createPacket(ack, seqRcvd)
                 print("ACK Packet to send : " + sentAck)
                 serverSocket.sendto(sentAck.encode('UTF-8'), clientAddress)
                 data = data.decode("UTF-8")
                 arr = data.split(",")
-                seqRcvd=1-seqRcvd
-                #print (seqRcvd)
-                return arr[2], clientAddress
+                seqRcvd = 1 - seqRcvd
+                # print (seqRcvd)
+                return arr[1], clientAddress
             serverSocket.sendto(sentAck.encode('UTF-8'), clientAddress)
             return receive()
 
@@ -66,7 +58,7 @@ while 1:
                 serverSocket.sendto(packet.encode('UTF-8'), clientAddress)
                 ack = serverSocket.recv(2048)
                 print("ACK received: " + ack.decode('UTF-8'))
-                if not (verifyCheckSum(ack) and isACK(ack)):
+                if not (isACK(ack)):
                     ack = serverSocket.recv(2048)
                 else:
                     seqSnd = 1 - seqSnd
@@ -74,13 +66,12 @@ while 1:
                 sendPacket(packet, clientAddress)
 
 
-        def getCheckSum(data):
-            return "koko"
+
 
 
         def createPacket(data, seq):
-            checkSum = getCheckSum(data)
-            packet = checkSum + ',' + str(seq) + ',' + str(data)
+
+            packet = str(seq) + ',' + str(data)
             return packet
 
 
